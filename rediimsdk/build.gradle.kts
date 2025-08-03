c16e1cbc9a72060782cd30fce92f3177c0b72038
Bruno Pizzani <48102928+bpizzani@users.noreply.github.com>
2025-08-03 14:50:53 +0200

    Create build.gradle.kts

---
+++ rediimsdk/build.gradle.kts
@@ -0,0 +1,29 @@
+plugins {
+    id("com.android.library") version "8.1.1"
+    kotlin("android") version "1.9.0"
+}
+
+android {
+    namespace = "com.rediim.sdk"
+    compileSdk = 34
+
+    defaultConfig {
+        minSdk = 21
+        targetSdk = 34
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
+
+dependencies {
+    implementation("com.squareup.okhttp3:okhttp:4.12.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
+    implementation("com.github.thumbmarkjs:thumbmark-android:1.0.+")
+}
