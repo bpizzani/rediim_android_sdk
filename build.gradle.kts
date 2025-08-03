4b95df76178813f4da42fa18f896ab64713fac45
Bruno Pizzani <48102928+bpizzani@users.noreply.github.com>
2025-08-03 15:16:58 +0200

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,6 +22,12 @@ android {
     }
 }
 
+repositories {
+    google()
+    mavenCentral()
+    maven(url = "https://jitpack.io")
+}
+
 dependencies {
     implementation("com.squareup.okhttp3:okhttp:4.12.0")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
