27686d6c18b58b4054556f029551dd0de8b1d4ca
Bruno Pizzani <48102928+bpizzani@users.noreply.github.com>
2025-09-14 17:34:34 +0200

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -54,7 +54,7 @@ afterEvaluate {
                 from(components["release"])
                 groupId = "com.github.bpizzani"
                 artifactId = "rediim_android_sdk"
-                version = "1.2.4" // Must match the Git tag exactly
+                version = "1.2.5" // Must match the Git tag exactly
             }
         }
     }
